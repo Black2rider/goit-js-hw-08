48934c1cecf7b2f5b14a2a60e79c1137bd81ee02
Black2rider <60221585+Black2rider@users.noreply.github.com>
2023-04-13 19:48:09 +0000

    Deploying to gh-pages from @ Black2rider/goit-js-hw-08@0111ee34d4a5e5807bbc2d46d03bba0689f86159 🚀

---
+++ 02-video.2ff02e9a.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("video-player");console.log(e);
+//# sourceMappingURL=02-video.2ff02e9a.js.map
---
+++ 02-video.c63fcd78.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("video-player");console.log(e)}();
+//# sourceMappingURL=02-video.c63fcd78.js.map
