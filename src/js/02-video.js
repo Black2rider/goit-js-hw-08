d80bd2e67f8c9738c090bdfa2152e5c237bb09df
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2023-07-18 22:56:07 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,16 @@
-const video = document.getElementById("video-player");
-console.log(video)
+import Player from '@vimeo/player';
+
+const player = new Player('vimeo-player', {
+});
+
+player.on('timeupdate', function(data) {
+  try {
+    let local = localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));  
+    local_.throttle(updatePosition, 100)
+  } catch(error) {
+    console.log(error.name);
+  }
+});
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+});
