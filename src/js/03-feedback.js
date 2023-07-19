03671059833a0f86a119caa0f1fa245c704cd55d
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2023-07-19 22:51:06 +0300

    3 start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+const form = document.querySelector("feedback-form");
+const LOKAL_KEY = "feedback-form-state";
 
+function saveForm(evt) {
+  evt.preventDefault();
+  form.addEventListener("input", saveForm);
+  localStorage.setItem(LOKAL_KEY, JSON.stringify(form));
+  
+}
