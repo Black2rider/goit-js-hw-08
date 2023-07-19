692225302ff1e0be19d168c85b7fab979494808f
Black2rider <60221585+Black2rider@users.noreply.github.com>
2023-07-19 19:51:38 +0000

    Deploying to gh-pages from @ Black2rider/goit-js-hw-08@03671059833a0f86a119caa0f1fa245c704cd55d 🚀

---
+++ 03-feedback.5a97d505.js
@@ -0,0 +1,2 @@
+document.querySelector("feedback-form");
+//# sourceMappingURL=03-feedback.5a97d505.js.map
---
+++ 03-feedback.b0b4323b.js
@@ -0,0 +1,2 @@
+document.querySelector("feedback-form");
+//# sourceMappingURL=03-feedback.b0b4323b.js.map
